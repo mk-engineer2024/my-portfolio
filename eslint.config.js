194101af53462d7a0740bcd4356530b83c367600
MK_PC_2025\ayase <mk.engineer2024@gmail.com>
2025-11-23 19:35:07 +0900

    ESLint v9蟇ｾ蠢・ eslint.config.js縺ｫ遘ｻ陦後゛SX縺ｨAstro繝輔ぃ繧､繝ｫ縺ｮ繝代・繧ｹ險ｭ螳壹ｒ菫ｮ豁｣

---
+++ eslint.config.js
@@ -0,0 +1,93 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import astroPlugin from 'eslint-plugin-astro';
+import astroParser from 'astro-eslint-parser';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+      },
+    },
+  },
+  {
+    files: ['**/*.{jsx,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/triple-slash-reference': 'off',
+    },
+  },
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: ['.astro'],
+      },
+    },
+    plugins: {
+      astro: astroPlugin,
+    },
+    rules: {
+      ...astroPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: [
+      'dist/',
+      '.astro/',
+      'node_modules/',
+      'package-lock.json',
+      'pnpm-lock.yaml',
+    ],
+  },
+];
+
